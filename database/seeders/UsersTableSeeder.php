1c8d061e6903a925b976bbc9b4cddaaed61b7d40
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-22 19:23:40 +0630

    bg

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,16 +14,16 @@ class UsersTableSeeder extends Seeder
 {
     public function run(): void
     {
-        $admin = $this->createUser(UserType::Admin, 'Owner', 'tigermm', '09123456789');
+        $admin = $this->createUser(UserType::Admin, 'Owner', 'linkhant', '09123456789');
         (new WalletService)->deposit($admin, 10 * 100_000, TransactionName::CapitalDeposit);
 
-        $master = $this->createUser(UserType::Master, 'Master', 'M898737', '0935234545', $admin->id);
+        $master = $this->createUser(UserType::Master, 'Master', 'LM898737', '0935234545', $admin->id);
         (new WalletService)->transfer($admin, $master, 5 * 100_000, TransactionName::CreditTransfer);
-        
-        $agent_1 = $this->createUser(UserType::Agent, 'Agent 1', 'A898737', '09112345674', $master->id, 'vH4HueE9');
+
+        $agent_1 = $this->createUser(UserType::Agent, 'Agent 1', 'L898737', '09112345674', $master->id, 'vH4HueE9');
         (new WalletService)->transfer($master, $agent_1, 5 * 100_00, TransactionName::CreditTransfer);
 
-        $player_1 = $this->createUser(UserType::Player, 'Player 1', 'P111111', '09111111111', $agent_1->id);
+        $player_1 = $this->createUser(UserType::Player, 'Player 1', 'L111111', '09111111111', $agent_1->id);
         (new WalletService)->transfer($agent_1, $player_1, 30000, TransactionName::CreditTransfer);
     }
 
@@ -33,7 +33,7 @@ class UsersTableSeeder extends Seeder
             'name' => $name,
             'user_name' => $user_name,
             'phone' => $phone,
-            'password' => Hash::make('delightmyanmar'),
+            'password' => Hash::make('password'),
             'agent_id' => $parent_id,
             'status' => 1,
             'referral_code' => $referral_code,
